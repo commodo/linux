fb954e1072a52a1c3992f6c07b442605128c2dfa
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-06-27 17:22:50 +0300

    drivers: jesd204: add jesd204_{to,from}_device() accessors
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- drivers/jesd204/jesd204-core.c
+++ drivers/jesd204/jesd204-core.c
@@ -68,6 +68,28 @@ struct jesd204_dev_state_data {
 	bool			inputs;
 };
 
+struct jesd204_dev *jesd204_dev_from_device(struct device *dev)
+{
+	struct jesd204_dev *jdev;
+
+	if (!dev)
+		return NULL;
+
+	list_for_each_entry(jdev, &jesd204_device_list, list) {
+		if (jdev->dev && jdev->dev == dev)
+			return jdev;
+	}
+
+	return NULL;
+}
+EXPORT_SYMBOL(jesd204_dev_from_device);
+
+struct device *jesd204_dev_to_device(struct jesd204_dev *jdev)
+{
+	return jdev ? jdev->dev : NULL;
+}
+EXPORT_SYMBOL(jesd204_dev_to_device);
+
 static struct jesd204_dev *jesd204_dev_alloc(struct device_node *np)
 {
 	struct jesd204_dev_top *jdev_top;
